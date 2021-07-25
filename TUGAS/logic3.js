9b4afdd76f4d2dc6a054181205f3fa00c42ba183
Erwin <Erwin165@gmail.com>
2021-07-25 08:23:53 +0700

    first commit

---
+++ TUGAS/logic3.js
@@ -0,0 +1,15 @@
+for(var y = 1; y < 10; y++) {
+    for(var x = 1; x <10; x++) {
+         if(y == x) {
+             document.write(' * ')
+         } else {
+             document.write(' - ')
+         }
+         if(x + y == 10) {
+            document.write(' * ')
+        } else {
+            document.write(' - ')
+        }
+    }
+    document.write('<br>');
+}
---
+++ coba.js
@@ -0,0 +1,5 @@
+var JumlahBarang = prompt("JUMLAH BARANG ANDA?",0);
+if(jumlahbarang > 5) {
+    document.write("<h2>selamat anda mendapatkan bonus</h2>");
+}
+document.write("<p>silahkan tambah lagi..</p>");
